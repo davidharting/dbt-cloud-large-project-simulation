a872dc8580af0eaf64865b15a357cccad8e2e958
dev@dbtlabs.com <dev@dbtlabs.com>
2023-09-12 17:09:56 +0000

    asdfaesfasdf

--- models/roll_up_models/rollup__model_one_hundred_and_forty_six.sql
+++ models/roll_up_models/rollup__model_one_hundred_and_forty_six.sql
@@ -1,7 +1,3 @@
-with model_a as
-  (select *
-   from {{ ref('core__model_two_hundred_and_fifty_two') }})
-select * exclude unqiue_key,
-         row_number() over (partition by 1
-                            order by 1) as unqiue_key
+with model_a as (select * from {{ ref("core__model_two_hundred_and_fifty_two") }})
+select * exclude unqiue_key, row_number() over (partition by 1 order by 1) as unqiue_key
 from model_a
